b2f94ff111ab3cf9545ae0a4f4effb25578459b0
ryazhisai <73305760+ryazhisai@users.noreply.github.com>
2021-04-09 08:55:26 -0700

    new proj

---
+++ view.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+#create a Flask instance
+app = Flask(__name__)
+
+@app.route('/')  # app routes to various html pages that we have assigned it to
+def home():
+    return render_template("test2.html")
+
+
+
+
+@app.route('/eventpage/')
+def eventpage():
+    return render_template("eventpage.html")
+
+
+if __name__ == "__main__":
+#runs the application on the repl development server
+    app.run(debug=True, port='5002')
+    app.run(debug=True, port="5001", host="127.0.0.1")
+
